90963f883ede3c8633fbf1c77d74728fec66e47f
SebastiaoFellipe <sebaslopes369@gmail.com>
2024-08-19 22:58:30 -0300

    Adicionando classe collision para gerenciar as colisões

---
+++ include/collision.h
@@ -0,0 +1,20 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include <memory>
+#include "player.h"
+#include "enemy.h"
+#include "base.h"
+#include "bullet.h"
+#include <SFML/Graphics.hpp>
+
+class Collision {
+public:
+    static bool checkBulletHitBase(std::shared_ptr<Bullet>& bullet, Base& base);
+    static bool checkBulletHitPlayer(std::shared_ptr<Bullet>& bullet, Player& player);
+    static bool checkEnemyHitPlayer(Enemy& enemy, Player& player);
+    static bool checkEnemyHitBase(Enemy& enemy, Base& base);
+    static bool checkPlayerBulletHitEnemy(std::shared_ptr<Bullet>& bullet, Enemy& enemy);
+};
+
+#endif // COLLISION_H
---
+++ src/collision.cpp
@@ -0,0 +1,21 @@
+#include "collision.h"
+
+bool Collision::checkBulletHitBase(std::shared_ptr<Bullet>& bullet, Base& base) {
+    return bullet->getBody().intersects(base.getBody());
+}
+
+bool Collision::checkBulletHitPlayer(std::shared_ptr<Bullet>& bullet, Player& player) {
+    return bullet->getBody().intersects(player.getBody());
+}
+
+bool Collision::checkEnemyHitPlayer(Enemy& enemy, Player& player) {
+    return enemy.getBody().intersects(player.getBody());
+}
+
+bool Collision::checkEnemyHitBase(Enemy& enemy, Base& base) {
+    return enemy.getBody().intersects(base.getBody());
+}
+
+bool Collision::checkPlayerBulletHitEnemy(std::shared_ptr<Bullet>& bullet, Enemy& enemy) {
+    return bullet->getBody().intersects(enemy.getBody());
+}
